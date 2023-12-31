64e5cef3286e39a0cc562bf9faf3abb0a2b5541b
defranciscon <108236585+defranciscon@users.noreply.github.com>
2023-12-31 09:48:48 -0500

    removed secret key strings from config.py

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ AUTH_USERS = 'authusers'
 
 class Config(object):
     'Base config class'
-    SECRET_KEY = '15acd65d26cfb615ed6983d2ba398b4b7f5038a9645c402e'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
 
 class ProdConfig(Config):
     'Production specific config'
-    SECRET_KEY = '8cfebfbaefb1be12f0bb5ace0fa74bef187e48519ea3d0d1'
+    SECRET_KEY = os.enviorn.get('SECRET_KEY')
 
 class StagingConfig(Config):
     'Staging specific config'
@@ -21,5 +21,5 @@ class StagingConfig(Config):
 class DevConfig(Config):
     'Development environment specific config'
     DEBUG = True
-    SECRET_KEY = 'ece7088abd8e19b9e11f28e10cc6077c333ce224e0af8c5b'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     
